3ac369e427750a9c8837540d14abe115af70c9ba
vakula16069 <vakula@nisum.com>
2018-01-31 14:47:46 +0530

    First commit

---
+++ src/main/java/com/vijay/mavenjavastandalone1/MainClass.java
@@ -0,0 +1,8 @@
+package com.vijay.mavenjavastandalone1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+	}
+}
---
+++ src/test/java/com/vijay/mavenjavastandalone1/MainClassTest.java
@@ -0,0 +1,14 @@
+package com.vijay.mavenjavastandalone1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
